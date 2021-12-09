324f5b5742635b047324a3ce29b10f09a43780e8
Kirikllz909 <kirikllz909@gmail.com>
2021-12-09 15:25:13 +0300

    Add some routes and todo

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Profile from "./Pages/Profile.jsx";
 
+import Profile from "./Pages/Profile.jsx";
+import Login from "./Pages/Login.jsx";
+import Register from "./Pages/Register.jsx";
+
+//TODO: Create routes for homepage, systemspage, historypage,personaldatapage
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<Profile />} path="/Profile" />
+                <Route element={<Profile />} path="/profile" />
+                <Route element={<Login />} path="/login" />
+                <Route element={<Register />} path="/register" />
             </Routes>
         </BrowserRouter>
     );
