3c9308d2843f46cd992303b13188d1894b6a46e0
Kirikllz909 <kirikllz909@gmail.com>
2021-12-09 15:24:58 +0300

    Change name from Home to Profile

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AdminNavigation } from "../Elements/AdminNavigation";
 import { Header } from "../Elements/Header";
 
-export default class Home extends React.Component {
+export default class Profile extends React.Component {
     render() {
         return (
             <div className="container-fluid">
