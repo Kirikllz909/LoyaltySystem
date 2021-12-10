ccad8638fbd15c2e4521a7c15d9016d923587d8c
Kirikllz909 <kirikllz909@gmail.com>
2021-12-10 17:07:15 +0300

    create fixed system option model

---
+++ server/models/fixed_system_option.model.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes) => {
+    const FixedSystemOption = sequelize.define("fixed_system_option", {
+        option_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        discount_value: { type: DataTypes.INTEGER },
+    });
+    return FixedSystemOption;
+};
