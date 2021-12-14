789e1057b788516cf7d92520b10d1277a3276fd2
Kirikllz909 <kirikllz909@gmail.com>
2021-12-15 01:38:45 +0300

    Allow null on one attrib and ban on others

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -7,21 +7,27 @@ module.exports = (sequelize, DataTypes) => {
         },
         login: {
             type: DataTypes.STRING,
+            allowNull: false,
         },
         password: {
             type: DataTypes.STRING,
+            allowNull: false,
         },
         email: {
             type: DataTypes.STRING,
+            allowNull: false,
         },
         role: {
             type: DataTypes.STRING,
+            allowNull: false,
         },
         balance: {
             type: DataTypes.INTEGER,
+            defaultValue: 0,
         },
         jwt_token: {
             type: DataTypes.STRING,
+            allowNull: true,
         },
     });
     return User;
