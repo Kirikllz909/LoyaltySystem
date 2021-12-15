36df7f9532fca7a25880b86e853e870c5a15affc
Kirikllz909 <kirikllz909@gmail.com>
2021-12-15 20:03:45 +0300

    add age and forbid empty values

--- server/models/personal_data.model.js
+++ server/models/personal_data.model.js
@@ -1,8 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
     const PersonalData = sequelize.define("personal_data", {
-        name: { type: DataTypes.STRING },
-        last_name: { type: DataTypes.STRING },
-        phone_number: { type: DataTypes.INTEGER },
+        name: { type: DataTypes.STRING, allowNull: false },
+        last_name: { type: DataTypes.STRING, allowNull: false },
+        age: { type: DataTypes.INTEGER, allowNull: false },
+        phone_number: { type: DataTypes.INTEGER, allowNull: false },
     });
     return PersonalData;
 };
