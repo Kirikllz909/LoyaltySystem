934b99f351516a1ca2340db2838f3a7274e02038
Kirikllz909 <kirikllz909@gmail.com>
2021-12-17 22:13:20 +0300

    Add controller for working with accamulative opt

---
+++ server/controllers/accamulativeOption.controller.js
@@ -0,0 +1,82 @@
+const db = require("../models");
+const AccamulativeSystemOption = db.accamulative_system_options;
+
+/**
+ *Create new accamulative system option by provided data
+ * @param {*} accamulativeOption Data for creating new option. Should contain step_value, purchase_exchange, score_rate_exchange, points_payment_limit_min, points_payment_limit_max, points_time_limit and systemId
+ * @returns newAccumulativeSystemOption
+ */
+
+exports.createAccamulativeOption = (accamulativeOption) => {
+    return AccamulativeSystemOption.create({
+        step_value: accamulativeOption.step_value,
+        purchase_exchange: accamulativeOption.purchase_exchange,
+        score_rate_exchange: accamulativeOption.score_rate_exchange,
+        points_payment_limit_min: accamulativeOption.points_payment_limit_min,
+        points_payment_limit_max: accamulativeOption.points_payment_limit_max,
+        points_time_limit: accamulativeOption.points_time_limit,
+        systemId: accamulativeOption.systemId,
+    })
+        .then((newAccumulativeSystemOption) => {
+            console.log(
+                ">> New accamulative system option was successfully created: " +
+                    JSON,
+                stringify(newAccumulativeSystemOption, null, 4)
+            );
+            return newAccumulativeSystemOption;
+        })
+        .catch((err) =>
+            console.log(
+                ">> Failed to create new accamulative system option: " + err
+            )
+        );
+};
+
+/**
+ *Update data of option by provided id
+ * @param {*} optionId Id of updating option
+ * @param {*} accamulativeOption Data for updating option. Should contain step_value, purchase_exchange, score_rate_exchange, points_payment_limit_min, points_payment_limit_max, points_time_limit and systemId
+ * @returns
+ */
+
+exports.updateAccamulativeOption = (optionId, accamulativeOption) => {
+    return AccamulativeSystemOption.update(
+        {
+            step_value: accamulativeOption.step_value,
+            purchase_exchange: accamulativeOption.purchase_exchange,
+            score_rate_exchange: accamulativeOption.score_rate_exchange,
+            points_payment_limit_min:
+                accamulativeOption.points_payment_limit_min,
+            points_payment_limit_max:
+                accamulativeOption.points_payment_limit_max,
+            points_time_limit: accamulativeOption.points_time_limit,
+            systemId: accamulativeOption.systemId,
+        },
+        { where: { option_id: optionId } }
+    )
+        .then((newAccamulativeOption) => {
+            console.log(
+                ">> New accamulative system option was successfully created: " +
+                    JSON.stringify(newAccamulativeOption, null, 4)
+            );
+            return newAccamulativeOption;
+        })
+        .catch((err) => {
+            console.log(">> Failed to create new accamulative option: " + err);
+        });
+};
+
+/**
+ * Delete option by provided id
+ * @param {*} optionId Id of option to delete
+ */
+
+exports.deleteAccumulativeOption = (optionId) => {
+    AccamulativeSystemOption.destroy(optionId)
+        .then(() => {
+            console.log(">> Option was successfully deleted");
+        })
+        .catch((err) => {
+            console.log(">> Failed to delete option: " + err);
+        });
+};
