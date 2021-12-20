454f8f85bb6e71ac948793345ea72f60f417c826
Kirikllz909 <kirikllz909@gmail.com>
2021-12-20 22:29:52 +0300

    Process different errors. Show result

--- server/Routes/Auth.js
+++ server/Routes/Auth.js
@@ -10,10 +10,24 @@ Router.post("/api/authService/login", async (req, res) => {
     const data = req.body;
     const { result, error } = await AuthService.login(data);
     if (error) {
-        res.status(400).send(error.details[0].message);
+        res.status(400).send(error.details ? error.details[0].message : error);
+    }
+    if (result) {
+        console.log(result.details[0].message);
+        res.status(200).send(result.details[0].message);
     }
 });
-Router.post("/api/authService/register", (req, res) => {});
+Router.post("/api/authService/register", async (req, res) => {
+    const data = req.body;
+    const { result, error } = await AuthService.register(data);
+    if (error) {
+        res.status(400).send(error.details ? error.details[0].message : error);
+    }
+    if (result) {
+        console.log(result.details[0].message);
+        res.status(201).send(result.details[0].message);
+    }
+});
 
 /**
  * export Router
