b92743cf307d9747db5c4a18150bd66814a5b8fb
Kirikllz909 <kirikllz909@gmail.com>
2022-01-26 19:40:42 +0300

    add new columns

--- server/models/purchase.model.js
+++ server/models/purchase.model.js
@@ -9,6 +9,16 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.INTEGER,
             allowNull: false,
         },
+        discount_amount: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+        },
+        total_amount: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+        },
         purchase_date: {
             type: DataTypes.DATE,
             defaultValue: DataTypes.NOW,
