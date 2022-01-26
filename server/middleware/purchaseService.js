265406c66349cc644a027a777ee0f6beeffe6dd4
Kirikllz909 <kirikllz909@gmail.com>
2022-01-26 15:02:34 +0300

    add service for purchases

---
+++ server/middleware/purchaseService.js
@@ -0,0 +1,93 @@
+const Joi = require("joi");
+const purchaseController = require("../controllers/purchase.controller");
+
+class PurchaseService {
+    validateData(data) {
+        const schema = Joi.object({
+            userId: Joi.number().min(1).required,
+            systemId: Joi.number().min(1),
+            purchase_amount: Joi.number().min(1),
+        });
+        return schema.validate(data);
+    }
+
+    async addUser(data) {
+        try {
+            const { error } = this.validateData(data);
+            if (error) {
+                return { error: "" + error };
+            }
+        } catch (error) {
+            return { error: "" + error };
+        }
+
+        try {
+            await purchaseController.createPurchase(
+                data.userId,
+                data.purchase_amount
+            );
+            return {
+                result: {
+                    details: [{ message: "Purchase was successfully created" }],
+                },
+            };
+        } catch (error) {
+            return { error: "" + error };
+        }
+    }
+
+    async deletePurchase(purchase_id) {
+        try {
+            await purchaseController.deletePurchase(purchase_id);
+            return {
+                result: {
+                    details: [{ message: "Purchase was successfully deleted" }],
+                },
+            };
+        } catch (error) {
+            return { error: "" + error };
+        }
+    }
+    async updatePurchase(purchase_id, data) {
+        try {
+            await purchaseController.updatePurchase(purchase_id, data);
+            return {
+                result: {
+                    details: [{ message: "Purchase was successfully updated" }],
+                },
+            };
+        } catch (error) {
+            return { error: "" + error };
+        }
+    }
+
+    async getUserPurchases(userId) {
+        try {
+            const purchases = await purchaseController.findAllUserPurchases(
+                userId
+            );
+            return {
+                result: {
+                    details: [{ message: purchases }],
+                },
+            };
+        } catch (error) {
+            return { error: "" + error };
+        }
+    }
+
+    async getPurchaseHistory() {
+        try {
+            const purchaseHistory = await purchaseController.findAllPurchases();
+            return {
+                result: {
+                    details: [{ message: purchaseHistory }],
+                },
+            };
+        } catch (error) {
+            return { error: "" + error };
+        }
+    }
+}
+
+module.exports = new PurchaseService();
