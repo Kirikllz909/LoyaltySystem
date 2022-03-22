ed1c59334138ba22235543be38c296bb905dbe9a
Kirikllz909 <kirikllz909@gmail.com>
2022-03-22 18:33:45 +0300

    update user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,29 +5,9 @@ module.exports = (sequelize, DataTypes) => {
             primaryKey: true,
             autoIncrement: true,
         },
-        login: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        role: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
         balance: {
             type: DataTypes.DOUBLE,
             defaultValue: 0,
-        },
-        jwt_token: {
-            type: DataTypes.STRING,
-            allowNull: true,
         },
     });
     return User;
